cb0f4132cbbb4b9778a346efc404fc55aa0381e3
Uchechukwu Okereke <85560317+uchebest40@users.noreply.github.com>
2022-04-15 20:37:22 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**print_triangle - check for a digit
+/**
+ * print_triangle - check for a digit
  * @size : integer type
  * Return:void
  */
@@ -12,11 +13,16 @@ void print_triangle(int size)
 
 	while (i <= size && size > 0)
 	{
-
 		ii = 0;
 		while (ii < size - i)
 		{
 			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
 			ii++;
 		}
 
